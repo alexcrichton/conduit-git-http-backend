4bdde0e6ac2116388e923bcc3ba03e2d23bde57a
Travis CI User <travis@example.org>
2017-08-29 13:10:53 +0000

    Documentation for alexcrichton/conduit-git-http-backend@024a32d

---
+++ implementors/conduit/trait.Handler.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["conduit_git_http_backend"] = ["impl Handler for <a class=\"struct\" href=\"conduit_git_http_backend/struct.Serve.html\" title=\"struct conduit_git_http_backend::Serve\">Serve</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["conduit_git_http_backend"] = {"doc":"","items":[[3,"Serve","conduit_git_http_backend","",null,null],[12,"0","","",0,null],[11,"call","","",0,{"inputs":[{"name":"self"},{"name":"request"}],"output":{"name":"result"}}]],"paths":[[3,"Serve"]]};
+initSearch(searchIndex);
